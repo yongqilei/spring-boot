476242f64b0b87f10a3811b1b8847564cdf53d86
Raja <2540356035@qq.com>
2018-08-10 15:20:10 +0800

    application.properties配置

--- springboot-activemq/src/main/java/com/raja/mq/ActivemqApplication.java
+++ springboot-activemq/src/main/java/com/raja/mq/ActivemqApplication.java
@@ -1,12 +1,23 @@
 package com.raja.mq;
 
+import javax.jms.Queue;
+
+import org.apache.activemq.command.ActiveMQQueue;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jms.annotation.EnableJms;
 
 @SpringBootConfiguration
-public class Application {
+@EnableJms
+public class ActivemqApplication {
 
+	@Bean
+	public Queue queue() {
+		return new ActiveMQQueue("sample.queue");
+	}
+	
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		SpringApplication.run(ActivemqApplication.class, args);
 	}
 }
