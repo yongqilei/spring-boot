0e56a34ff8e3718009a6d0734ce0c6572a095260
Raja <2540356035@qq.com>
2018-08-10 14:57:40 +0800

    忽略配置，和Spring boot Activemq的启动类

---
+++ springboot-activemq/src/main/java/com/raja/mq/Application.java
@@ -0,0 +1,12 @@
+package com.raja.mq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
+
+@SpringBootConfiguration
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
